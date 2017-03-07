2ab5e5697a4ce866055b4b1f1a0e3e3467776ff6
leader22 <y.sugiura.0316@gmail.com>
2017-03-07 09:48:46 +0900

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const search = require('./lib/search');
+const version = require('./lib/version');
+
+const argv = require('yargs')
+  .usage('Usage: $0 -b v0.40')
+  .default('b', 'master')
+  .alias('b', 'branch')
+  .alias('v', 'version')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
+if (argv.v) {
+  version();
+  process.exit(0);
+}
+
+search(argv.branch);
