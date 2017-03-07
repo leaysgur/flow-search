f79e9b4a74f736bc1c9835df3abc843c13097597
leader22 <y.sugiura.0316@gmail.com>
2017-03-07 11:45:42 +0900

    find

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-const search = require('./lib/search');
 const version = require('./lib/version');
+const search = require('./lib/search');
+const find = require('./lib/find');
 
 const argv = require('yargs')
   .usage('Usage: $0 -b v0.40')
   .default('b', 'master')
   .alias('b', 'branch')
   .alias('v', 'version')
+  .alias('d', 'declare')
   .help('h')
   .alias('h', 'help')
   .argv;
+
 
 if (argv.v) {
   return version().then(console.log);
 }
 
-const [ keyword ] = argv._;
-if (keyword) {
-  return search(argv.branch).then(res => find(res, keyword));
+if (argv.d) {
+  // for DEBUG
+  const res = require('fs').readFileSync('./decls.txt', 'utf8');
+  return find(res, argv.d).then(console.log);
+  // return search(argv.branch)
+  //   .then(res => find(res, argv.d))
+  //   .then(console.log);
 }
 
 return search(argv.branch).then(console.log);
