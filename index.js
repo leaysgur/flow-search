41498f8ef66dedd4d0a4ebec7840034563cabcf6
leader22 <y.sugiura.0316@gmail.com>
2017-03-07 11:56:05 +0900

    refactor

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-const version = require('./lib/version');
 const search = require('./lib/search');
 const find = require('./lib/find');
 
 const argv = require('yargs')
-  .usage('Usage: $0 -b v0.40')
-  .default('b', 'master')
-  .alias('b', 'branch')
-  .alias('v', 'version')
-  .alias('d', 'declare')
+  .usage('Usage: $0 [options]')
   .help('h')
   .alias('h', 'help')
+  .version()
+  .alias('v', 'version')
+  .default('b', 'master')
+  .default('d', '')
+  .alias('b', 'branch')
+  .alias('d', 'declare')
   .argv;
 
 
-if (argv.v) {
-  return version().then(console.log);
-}
-
 if (argv.d) {
-  // for DEBUG
-  const res = require('fs').readFileSync('./decls.txt', 'utf8');
-  return find(res, argv.d).then(console.log);
-  // return search(argv.branch)
-  //   .then(res => find(res, argv.d))
-  //   .then(console.log);
+  return search(argv.branch)
+    .then(res => find(res, argv.d))
+    .then(console.log);
 }
 
 return search(argv.branch).then(console.log);
